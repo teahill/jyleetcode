0b0d547bae047ee6f4f38b0fae3be250dd9f5f49
teahill <teahill@outlook.com>
2015-03-16 21:10:29 -0700

    added solutions for problem 71

---
+++ src/com/leetcode/jyang/LC071_SimplifyPath.java
@@ -0,0 +1,20 @@
+package com.leetcode.jyang;
+
+/**
+ * Given an absolute path for a file (Unix-style), simplify it.
+ * 
+ * For example,
+ * path = "/home/", => "/home"
+ * path = "/a/./b/../../c/", => "/c"
+ * 
+ * @author jyang
+ *
+ */
+
+public class LC071_SimplifyPath {
+
+    public String simplifyPath(String path) {
+        
+    }
+	
+}
