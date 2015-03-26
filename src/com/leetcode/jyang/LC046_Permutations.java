2910ca38248753bb27d4a81ca566b2e247171512
Jun Yang <jyang@ebay.com>
2015-03-25 22:09:49 -0700

    adding partial solution for 046

--- src/com/leetcode/jyang/LC046_Permutations.java
+++ src/com/leetcode/jyang/LC046_Permutations.java
@@ -1,5 +1,6 @@
 package com.leetcode.jyang;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,4 +23,41 @@ public class LC046_Permutations {
 	        
 	}
 	
+	private List<List<String>> internal(int[] n){
+		
+		List<List<String>> res = new ArrayList<List<String>>();
+		
+		if (n.length==0){
+			return res;
+		}
+		
+		for (int i=0; i<n.length; i++){
+			
+			int anchor = n[i];
+			
+			int[] remain = partition(n, i);
+			
+			
+
+			
+		}
+		
+		
+	}
+	
+	private int[] partition(int[] n, int pos){
+		
+		int[] r = new int[n.length-1];
+		
+		for (int i=0; i<n.length; i++){		
+			if (i<pos){
+				r[i] = n[i];
+			}
+			else if (i>pos){
+				r[i-1] = n[i];
+			}
+		}
+		
+		return r;
+	}
 }
