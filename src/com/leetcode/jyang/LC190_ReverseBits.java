b4d61620b942f60b8ae66be96ae1772a59551843
jyang <jyang@ebay.com>
2015-04-02 11:33:46 -0700

    adding partial solutions for 165

--- src/com/leetcode/jyang/LC190_ReverseBits.java
+++ src/com/leetcode/jyang/LC190_ReverseBits.java
@@ -20,6 +20,18 @@ package com.leetcode.jyang;
  */
 
 public class LC190_ReverseBits {
+	
+    public int reverseBits(int n) {
+    	
+        long sum=0;
+        
+        for(int i=0;i<32;i++){
+            sum += Math.pow(2,31-i)*(n&1);
+            n = n>>1;
+        }
+        
+        return (int)sum;       
+    }
 
     // you need treat n as an unsigned value
     public long reverseBits(long n) {
