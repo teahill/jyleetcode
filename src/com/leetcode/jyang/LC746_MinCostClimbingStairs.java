713c54f8bc89da775615692256b00c02e4850b56
Jun <jyang727@gmail.com>
2019-03-25 22:59:51 -0700

    added LC746

---
+++ src/com/leetcode/jyang/LC746_MinCostClimbingStairs.java
@@ -0,0 +1,13 @@
+package com.leetcode.jyang;
+
+/**
+ * 
+ * https://leetcode.com/problems/min-cost-climbing-stairs/
+ * 
+ * @author jyang
+ *
+ */
+
+public class LC746_MinCostClimbingStairs {
+
+}
